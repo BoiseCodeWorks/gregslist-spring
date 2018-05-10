3a091ae92910fd89caf1006f7a23ca6c9f08bc5f
Jake Overall <joverall22@gmail.com>
2018-05-10 11:16:02 -0600

    Jasper

--- app/CarService.js
+++ app/CarService.js
@@ -1,54 +1,53 @@
-function CarService(cb){
+function CarService(cb) {
   //This is where the data lives
   var baseUrl = "https://bcw-gregslist.herokuapp.com/api/cars"
   //PRIVATE
   var cars = []
-  
 
-  function Car(img, year, model, make, price, description){
+
+  function Car(img, year, model, make, price, description) {
     this.imgUrl = img || "//placehold.it/200x200"
     this.year = year
     this.model = model
     this.make = make
     this.price = price
     this.description = description || "No description provided"
-   }
+  }
 
 
-  function loadCars(){
-    $.get(baseUrl).then(res=>{
+  function loadCars() {
+    $.get(baseUrl).then(res => {
       cb(res.data)
     })
   }
-loadCars()
+  loadCars()
   //PUBLIC
-  this.getCars = function getCars(){
-    return JSON.parse(JSON.stringify(cars))
-  }
 
-  this.addCar = function addCar(car){
+  this.addCar = function addCar(car) {
     var newCar = new Car(car.img, car.year, car.model, car.make, car.price)
     $.post(baseUrl, newCar)
-    .then(res=>{
-      loadCars()
-    })
+      .then(res => {
+        loadCars()
+      })
   }
 
   this.deleteCar = function deleteCar(id) {
-    $.ajax ({
+    $.ajax({
       method: 'DELETE',
       url: baseUrl + '/' + id
     }).then(res => {
       loadCars()
     })
   }
-  this.discountCar = function discountCar(id, price){
-       $.ajax({
+  this.discountCar = function discountCar(id, price) {
+    $.ajax({
       method: 'PUT',
       url: baseUrl + '/' + id,
       contentType: 'application/JSON',
-      data: JSON.stringify({price: price * .9})
-    }).then(res =>{
+      data: JSON.stringify({
+        price: price * .9
+      })
+    }).then(res => {
       loadCars()
     })
   }
